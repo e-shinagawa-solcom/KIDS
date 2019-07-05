7d302ff027a8e182bec4a304250b7fddba4f800a
maito osa <solcom-develop@solcom-net.co.jp>
2019-07-05 15:15:18 +0900

    請求管理メニューの追加

---
+++ 01_kidsweb/home/kids2/src/layout/type01/navi/inv/images.js
@@ -0,0 +1,15 @@
+
+	///// REGISTRATION NAVI BT IMAGE /////
+	var regist1 = '/img/type01/inv/regist_off_bt.gif';
+	var regist2 = '/img/type01/inv/regist_off_on_bt.gif';
+	var regist3 = '/img/type01/inv/regist_on_bt.gif';
+
+	///// SEARCH NAVI BT IMAGE /////
+	var search1 = '/img/type01/inv/search_off_bt.gif';
+	var search2 = '/img/type01/inv/search_off_on_bt.gif';
+	var search3 = '/img/type01/inv/search_on_bt.gif';
+
+	///// TOTAL NAVI BT IMAGE /////
+	var total1 = '/img/type01/inv/total_off_bt.gif';
+	var total2 = '/img/type01/inv/total_off_on_bt.gif';
+	var total3 = '/img/type01/inv/total_on_bt.gif';
---
+++ 01_kidsweb/home/kids2/src/layout/type01/navi/inv/initlayout.js
@@ -0,0 +1,14 @@
+
+
+///// REGISTRATION NAVI BT IMAGE /////
+var registnavi1 = '<a href="#"><img onmousedown="fncAlphaOn( this );" onmouseup="fncAlphaOff( this );" onmouseover="RegistOn(this)" onmouseout="RegistOff(this);fncAlphaOff( this );" src="' + regist1 + '" width="151" height="25" border="0" alt="ÀÁµá½ñÅÐÏ¿"></a>';
+var registnavi3 = '<img src="' + regist3 + '" width="151" height="25" border="0" alt="ÀÁµá½ñÅÐÏ¿">';
+
+
+///// SEARCH NAVI BT IMAGE /////
+var searchnavi1 = '<a href="#"><img onmousedown="fncAlphaOn( this );" onmouseup="fncAlphaOff( this );" onmouseover="SearchOn(this);" onmouseout="SearchOff(this);fncAlphaOff( this );" src="' + search1 + '" width="151" height="25" border="0" alt="ÀÁµá½ñ¸¡º÷"></a>';
+var searchnavi3 = '<img src="' + search3 + '" width="151" height="25" border="0" alt="ÀÁµá½ñ¸¡º÷">';
+
+///// TOTAL NAVI BT IMAGE /////
+var totalnavi1 = '<a href="#"><img onmousedown="fncAlphaOn( this );" onmouseup="fncAlphaOff( this );" onmouseover="TotalOn(this);" onmouseout="TotalOff(this);fncAlphaOff( this );" src="' + total1 + '" width="151" height="25" border="0" alt="ÀÁµá½¸·×"></a>';
+var totalnavi3 = '<img src="' + total3 + '" width="151" height="25" border="0" alt="ÀÁµá½¸·×">';
