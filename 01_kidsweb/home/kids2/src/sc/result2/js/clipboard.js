e1b6724e569e36ad742cff34afa791f746c10837
Tetsuo Miyata <miyata@basystems.co.jp>
2019-08-31 12:16:21 +0900

    [update] クリップボードコピーがIEでしか動かない実装だったのでchromeとedgeで動くよう実装を変更

--- 01_kidsweb/home/kids2/src/sc/result2/js/clipboard.js
+++ 01_kidsweb/home/kids2/src/sc/result2/js/clipboard.js
@@ -9,12 +9,45 @@ var records = table.find('tbody > tr');
 // 区切り文字
 var delimiter = '\t';
 
+// 任意の文字列をクリップボードにコピーする
+function execCopy(string){
+
+    // 空div 生成
+    var tmp = document.createElement("div");
+    // 選択用のタグ生成
+    var pre = document.createElement('pre');
+  
+    // 親要素のCSSで user-select: none だとコピーできないので書き換える
+    pre.style.webkitUserSelect = 'auto';
+    pre.style.userSelect = 'auto';
+  
+    tmp.appendChild(pre).textContent = string;
+  
+    // 要素を画面外へ
+    var s = tmp.style;
+    s.position = 'fixed';
+    s.right = '200%';
+  
+    // body に追加
+    document.body.appendChild(tmp);
+    // 要素を選択
+    document.getSelection().selectAllChildren(tmp);
+  
+    // クリップボードにコピー
+    var result = document.execCommand("copy");
+  
+    // 要素削除
+    document.body.removeChild(tmp);
+  
+    return result;
+ };
+ 
 // コピーボタン
 $('img.copy').on({
     'click': function(){
         // クリップボードに反映させる文字列
         var contents = "";
-
+        
         // ヘッダ行の取得
         headers.each(function(){
             contents += $(this).children('div').get(0).innerHTML + delimiter;
@@ -34,11 +67,20 @@ $('img.copy').on({
         });
 
         // クリップボードに値を反映
-        if (window.clipboardData.setData('Text', contents)){
+        // 既存コードはIEでしか動かないため実装を変更（2019/8/31 T.Miyata）
+        //if (window.clipboardData.setData('Text', contents)){
+        //    alert("クリップボードに検索結果をコピーしました。");
+        //}
+        //else {
+        //   alert("クリップボードへのコピーに失敗しました。");
+        //}
+        if (execCopy(contents)){
             alert("クリップボードに検索結果をコピーしました。");
-        }
-        else {
+        }else{
             alert("クリップボードへのコピーに失敗しました。");
         }
+
     }
+
+    
 });
