fccc129d48a18644d7dd608d8a9251910e4416d1
maito osa <solcom-develop@solcom-net.co.jp>
2019-12-23 13:46:48 +0900

    障害552の修正対応

--- 01_kidsweb/home/kids2/src/po/finish/functions.js
+++ 01_kidsweb/home/kids2/src/po/finish/functions.js
@@ -1,7 +1,13 @@
 
-(function(){
+(function () {
     // 閉じた際の処理
-    $(window).on('beforeunload', function(){
+    $(window).on('beforeunload', function () {
         $(window.opener.opener.document).find('form').submit();
     });
+
+
+    function fncListOutput(strURL) {
+        listW = window.open(strURL, 'listWin', 'width=800,height=600,top=10,left=10,status=yes,scrollbars=yes,directories=no,menubar=yes,resizable=yes,location=no,toolbar=no');
+        return false;
+    }
 })();
