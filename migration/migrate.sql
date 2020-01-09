9bcaa372902cbd3b34c64c892ec00f9cdb817022
k_nakajima <43240263kn-akajima@users.noreply.github.com>
2020-01-09 16:23:42 +0900

    LC移行追加

--- migration/migrate.sql
+++ migration/migrate.sql
@@ -1,4 +1,4 @@
-/*
+
 \i 1_mig_product.sql;
 \qecho 'step1 completed'
 \i 2_mig_order.sql;
@@ -12,7 +12,10 @@
 --\i 6_update_m_stockitem.sql;
 \i 7_mig_stock.sql;
 \qecho  'step7 completed'
-*/
 \i 8_mig_mold.sql;
 \qecho  'step8 completed'
+
+\i mig_lc.sql
+\i mig_lc_2.sql
+
 --\i m_slipkindrelation.sql;\
