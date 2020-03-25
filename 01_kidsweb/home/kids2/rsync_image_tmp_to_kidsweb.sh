d8195fe4a70baf5e73497302966a47ff3cb469e2
maito osa <solcom-develop@solcom-net.co.jp>
2020-03-25 09:30:02 +0900

    kidsdbに動くバッチのコミット

---
+++ 01_kidsweb/home/kids2/rsync_image_tmp_to_kidsweb.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# ****************************************************************************
+#
+# K.I.D.S. Open RSYNC
+#
+# Producer: Kazushi Saito
+# MakeDate: 2008/09/23
+# EditDate:
+# Note:
+#     > crontab setting sample.
+#       0 4 * * * /home/kids2/rsync_image_tmp.sh > /dev/null
+#
+#     > REMOTE -> LOCAL  RSYNC
+#
+#
+# ****************************************************************************
+
+TARGET_SERVER="192.168.10.218"
+REMOTE_LOGIN_USER="kids2"
+
+REMOTE_PATH="/home/kids2/"
+LOCAL_PATH="/home/kids2/"
+
+# TARGET 1 upfiles/
+SRC_DIR=${LOCAL_PATH}"src/p/edit/image_tmp/"
+DEST_DIR=${REMOTE_PATH}"src/p/edit/image_tmp/"
+ID_DSA=${LOCAL_PATH}".ssh/id_dsa.passno"
+
+#rsync -avz  -e "ssh -i ${ID_DSA}" ${SRC_DIR}  ${REMOTE_LOGIN_USER}@${TARGET_SERVER}:${DEST_DIR}
+rsync -avz  -e "ssh -i ${ID_DSA}" ${REMOTE_LOGIN_USER}@${TARGET_SERVER}:${DEST_DIR}  ${SRC_DIR}
+
+
+
+
